19cec5e3434f138b6afa8ff7f1242af7e6324c6a
Justin T Calkins <calkij@rpi.edu>
2016-02-06 00:03:47 -0500

    Initial Commit
    Just the basic stuff to get it all started.  We'll change a lot along
    the way but this is the beginning of it all.

---
+++ TheWedding/TheWedding.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace TheWedding.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Pictures()
+        {
+            var model = new Dictionary<string, List<string>>();
+            model.Add("Engagement Photos", new List<string>() { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" });
+            model.Add("Wedding Photos", new List<string>() { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" });
+            return View(model);
+        }
+
+        public ActionResult RSVP()
+        {
+            return View();
+        }
+    }
+}
---
+++ TheWedding/TheWedding.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TheWedding.WebUI.MvcApplication" Language="C#" %>
